5310e3ff34cf949bb9c3b1cb1133e8778f655bc0
sezerogras <ograssezer474@gmail.com>
2025-06-01 16:50:02 +0300

    edit bot name

--- web/i18n/tr-TR/common.ts
+++ web/i18n/tr-TR/common.ts
@@ -554,7 +554,7 @@ const translation = {
       vectorHash: 'Vektör Hash:',
       hitScore: 'Geri Alım Skoru:',
     },
-    inputPlaceholder: 'ThinkAI chat ile sohbet et',
+    inputPlaceholder: 'Marmus chat ile sohbet et',
     thought: 'Düşünce',
     thinking: 'Düşünü...',
     resend: 'Yeniden gönder',
